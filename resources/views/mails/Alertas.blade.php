329fac876be3cf4e5abdd1720cc8989688ad36e3
Delvis <ddavimo@hotmail.com>
2021-09-01 00:37:36 -0400

    Inicial

--- resources/views/mails/Alertas.blade.php
+++ resources/views/mails/Alertas.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <h2>Prueba de correo</h2>
-    <p>Este es el primer correo que envio desde laravel</p>
+    <p>Correo de alerta </p>
 </head>
 <body>
     
