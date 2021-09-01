584674583d898b3bdcf56827e8668464c69f6e5c
Delvis <ddavimo@hotmail.com>
2021-09-01 17:27:31 -0400

    RamaExp1

--- resources/views/mails/Alertas.blade.php
+++ resources/views/mails/Alertas.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <h2>Prueba de correo</h2>
-    <p>Correo de alerta </p>
+    <p>Correo de alerta en rama experimental </p>
 </head>
 <body>
     
